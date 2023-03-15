fe43730a98f347e71ebdbec0e6f4c3fda623c083
THEVOL <gabriel_calixto.gsc@hotmail.com>
2023-03-14 22:48:48 -0300

    Alterações feitas apra tratar a entrada do usuário e loop para comparar com a palavra secreta

--- forca.py
+++ forca.py
@@ -5,11 +5,21 @@ def jogar():
 
     palavra_secreta = "banana"
 
-	enforcou = False
-	acertou = False
+    enforcou = False
+    acertou = False
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
+    while(not enforcou and not acertou):
+
+        chute = input("Tente uma letra")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} a posição {}".format(chute, index))
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim do jogo")
 
