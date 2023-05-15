6bbcae9764fde34427a438216221e1e5e83f77fb
Gabriel Calixto <gabriel_calixto.gsc@hotmail.com>
2023-05-15 12:09:09 -0300

    Ajustanto capitalização das palavras e adicionando mecânica de erros.

--- forca.py
+++ forca.py
@@ -3,25 +3,35 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavraSecreta = "banana"
-    letrasAcertadas = ["_", "_","_", "_", "_", "_"]
+    palavraSecreta = "banana".upper()
+    letrasAcertadas = ["_" for letra in palavraSecreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while(not enforcou and not acertou):
 
         chute = input("Tente uma letra")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavraSecreta:
-            if(chute.upper() == letra.upper()):
-                letrasAcertadas[index] = letra
-            index = index + 1
+        if(chute in palavraSecreta):
+            index = 0
+            for letra in palavraSecreta:
+                if(chute == letra):
+                    letrasAcertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letrasAcertadas
         print(letrasAcertadas)
 
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 
