3114925df4dd7406ee515669ee91af2b9f9b13df
Gabriel Calixto <gabriel_calixto.gsc@hotmail.com>
2023-05-19 11:36:13 -0300

    organizando em funções e ajustes na nomeclatura de variáveis

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,39 +1,27 @@
 import random
 
 def jogar():
-    print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************")
+    imprimeApresentação()
 
-    numero_secreto = random.randrange(1, 101)
-    total_de_tentativas = 0
+    numeroSecreto = random.randrange(1, 101)
+    totalDeTentativas = 0
     pontos = 10000
 
-    print("Qual é o nível de dificuldade desejado?")
-    print("(1) para fácil \n(2) para médio \n(3) para difícil")
+    escolheNivel(totalDeTentativas)
 
-    nivel = int(input("Defina o nível: "))
-
-    if (nivel == 1):
-        total_de_tentativas = 20
-    elif (nivel == 2):
-        total_de_tentativas = 10
-    else:
-        total_de_tentativas = 5
-
-    for rodada in range(1, total_de_tentativas + 1):
-        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-        chute_str = input("Digite um número entre 1 e 100: ")
-        print("Você digitou " , chute_str)
-        chute = int(chute_str)
+    for rodada in range(1, totalDeTentativas + 1):
+        print("Tentativa {} de {}".format(rodada, totalDeTentativas))
+        chuteStr = input("Digite um número entre 1 e 100: ")
+        print("Você digitou " , chuteStr)
+        chute = int(chuteStr)
 
         if(chute < 1 or chute > 100):
             print("Você deve digitar um número entre 1 e 100!")
             continue
 
-        acertou = chute == numero_secreto
-        maior   = chute > numero_secreto
-        menor   = chute < numero_secreto
+        acertou = chute == numeroSecreto
+        maior   = chute > numeroSecreto
+        menor   = chute < numeroSecreto
 
         if(acertou):
             print("Você acertou e fez {} pontos!".format(pontos))
@@ -45,10 +33,28 @@ def jogar():
                 print("Você errou! O seu chute foi menor do que o número secreto.")
 
             #calculo de pontos com numeros absolutos
-            pontos_perdidos = abs(numero_secreto - chute)
-            pontos = pontos - pontos_perdidos
+            pontosPerdidos = abs(numeroSecreto - chute)
+            pontos = pontos - pontosPerdidos
 
     print("Fim do jogo")
+
+def imprimeApresentação():
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+def escolheNivel(totalDeTentativas):
+    print("Qual é o nível de dificuldade desejado?")
+    print("(1) para fácil \n(2) para médio \n(3) para difícil")
+
+    nivel = int(input("Defina o nível: "))
+
+    if (nivel == 1):
+        totalDeTentativas = 20
+    elif (nivel == 2):
+        totalDeTentativas = 10
+    else:
+        totalDeTentativas = 5
 
 if(__name__ == "__main__"):
     jogar()
